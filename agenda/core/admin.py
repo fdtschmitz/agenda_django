ccd753b3e0053ebe0f895871a8a18bdcc7a20e45
fdtschmitz <fdtschmitz.dev@gmail.com>
2024-03-18 08:30:07 -0300

    Criação do projeto Agenda e banco de dados

---
+++ agenda/core/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from core.models import Evento
+
+# Register your models here.
+
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao', 'usuario')
+    list_filter = ('usuario', 'data_evento')
+
+
+admin.site.register(Evento, EventoAdmin)
+
