34b97fdd97237d91a01341aedc17af0de95e1800
fdtschmitz <fdtschmitz.dev@gmail.com>
2024-03-19 06:37:48 -0300

    Inclusão do readme.md

--- agenda/core/models.py
+++ agenda/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
 
@@ -16,4 +17,7 @@ class Evento(models.Model):
 
     def __str__(self):
         return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y - %H:%M hrs')
 
